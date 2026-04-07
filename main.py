441e3e20bb0b0965ce625866f7bc800966bc7647
Anhelina <beisiukangelina@MacBook-Air-Bejsuk.local>
2026-04-07 16:51:43 +0300

    feat: final base logic for person A.

--- main.py
+++ main.py
@@ -1,36 +1,31 @@
 
 menu = []
 
+def add_dish():
+    name = input("Назва страви: ")
+    try:
+        price = float(input("Ціна: "))
+    except ValueError:
+        print("Помилка! Введіть число.")
+        return
+    descr = input("Опис: ")
+    menu.append({"назва": name, "ціна": price, "опис": descr})
+    print(f"Страву {name} додано!")
+
+def show_menu():
+    print("\n--- МЕНЮ РЕСТОРАНУ ---")
+    if not menu:
+        print("Меню порожнє.")
+    for dish in menu:
+        print(f"{dish['назва']} | {dish['ціна']} грн | {dish['опис']}")
 
 def main():
     while True:
-        print("\n--- МЕНЮ РЕСТОРАНУ ---")
-        print("1. Додати страву (A)")
-        print("2. Редагувати страву (Б)")
-        print("3. Видалити страву (В)")
-        print("4. Показати список страв (A)")
-        print("5. Загальна ціна (Г)")
-        print("0. Вихід")
-
-        choice = input("\nОберіть дію: ")
-
-        if choice == '1':
-            add_dish()
-        elif choice == '4':
-            show_menu()
-        elif choice == '0':
-            break
-
-
-def add_dish():
-
-    pass
-
-
-def show_menu():
-
-    pass
-
+        print("\n1. Додати страву (А)\n4. Показати меню (А)\n0. Вихід")
+        choice = input("Оберіть дію: ")
+        if choice == '1': add_dish()
+        elif choice == '4': show_menu()
+        elif choice == '0': break
 
 if __name__ == "__main__":
     main()
